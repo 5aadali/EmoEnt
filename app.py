8247a30b369a2efc82f1d6ed58467a6fa6604d1b
Syed Daniyal Hashmi <110484391+Daniyalhash@users.noreply.github.com>
2024-08-01 19:38:02 +0500

    Updated

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import cv2
 import numpy as np
 import mediapipe as mp
 from keras.models import load_model
-import webbrowser
 import os
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS
+
 app.secret_key = 'supersecretkey'  # Set a secret key for session management
 
 model = load_model("model.h5")
@@ -20,44 +22,34 @@ drawing = mp.solutions.drawing_utils
 if not os.path.exists("emotion.npy"):
     np.save("emotion.npy", np.array([""]))
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    try:
-        emotion = np.load("emotion.npy")[0]
-    except FileNotFoundError:
-        emotion = ""
+@app.route('/api/capture_emotion', methods=['POST'])
+def api_capture_emotion():
+    capture_emotion()
+    emotion = np.load("emotion.npy")[0]
+    return jsonify({"emotion": emotion})
+@app.route('/api/recommend', methods=['POST'])
+def api_recommend():
+    lang = request.json.get('language')
+    singer = request.json.get('singer')
+    emotion = np.load("emotion.npy")[0]
+    if not emotion:
+        return jsonify({"warning": "Please let me capture your emotion first"})
+    else:
+        # Construct recommendation URL
+        search_query = f"{lang} {emotion} song {singer}"
+        url = f"https://www.youtube.com/results?search_query={search_query}"
+        np.save("emotion.npy", np.array([""]))
+        return jsonify({"url": url})
 
-    lang = session.get('language', '')
-    singer = session.get('singer', '')
-
-    if request.method == 'POST':
-        lang = request.form['language']
-        singer = request.form['singer']
-        session['language'] = lang
-        session['singer'] = singer
-
-        if 'capture' in request.form:
-            capture_emotion()
-        elif 'recommend' in request.form:
-            emotion = np.load("emotion.npy")[0]
-            if not emotion:
-                warning = "Please let me capture your emotion first"
-                return render_template('index.html', warning=warning, language=lang, singer=singer)
-            else:
-                webbrowser.open(f"https://www.youtube.com/results?search_query={lang}+{emotion}+song+{singer}")
-                np.save("emotion.npy", np.array([""]))
-                session.pop('language', None)
-                session.pop('singer', None)
-                emotion = ""
-
-    return render_template('index.html', emotion=emotion, language=lang, singer=singer)
 
 def capture_emotion():
     cap = cv2.VideoCapture(0)
+    
     while True:
         ret, frame = cap.read()
         if not ret:
             break
+        
         frame = cv2.flip(frame, 1)
         res = holis.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         lst = []
@@ -67,26 +59,24 @@ def capture_emotion():
                 lst.append(i.x - res.face_landmarks.landmark[1].x)
                 lst.append(i.y - res.face_landmarks.landmark[1].y)
 
-            if res.left_hand_landmarks:
-                for i in res.left_hand_landmarks.landmark:
-                    lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
-                    lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
-            else:
-                for i in range(42):
-                    lst.append(0.0)
+        if res.left_hand_landmarks:
+            for i in res.left_hand_landmarks.landmark:
+                lst.append(i.x - res.left_hand_landmarks.landmark[8].x)
+                lst.append(i.y - res.left_hand_landmarks.landmark[8].y)
+        else:
+            lst.extend([0.0] * 42)  # Append zeros if landmarks are missing
 
-            if res.right_hand_landmarks:
-                for i in res.right_hand_landmarks.landmark:
-                    lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
-                    lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
-            else:
-                for i in range(42):
-                    lst.append(0.0)
+        if res.right_hand_landmarks:
+            for i in res.right_hand_landmarks.landmark:
+                lst.append(i.x - res.right_hand_landmarks.landmark[8].x)
+                lst.append(i.y - res.right_hand_landmarks.landmark[8].y)
+        else:
+            lst.extend([0.0] * 42)  # Append zeros if landmarks are missing
 
-            lst = np.array(lst).reshape(1, -1)
-            pred = label[np.argmax(model.predict(lst))]
-            np.save("emotion.npy", np.array([pred]))
-            break
+        lst = np.array(lst).reshape(1, -1)
+        pred = label[np.argmax(model.predict(lst))]
+        np.save("emotion.npy", np.array([pred]))
+        break
     
     cap.release()
     cv2.destroyAllWindows()
